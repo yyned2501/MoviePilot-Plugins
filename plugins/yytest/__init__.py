194d64771e106b8b86e9e1c0f1c063c39ece8158
杨旸 <yyned2501@vip.qq.com>
2024-03-29 11:12:33 +0800

    测试辅助识别

--- plugins/yytest/__init__.py
+++ plugins/yytest/__init__.py
@@ -5,6 +5,8 @@ from app.core.event import eventmanager, Event
 from app.log import logger
 from app.plugins import _PluginBase
 from app.schemas.types import EventType
+from app.modules.themoviedb.tmdbv3api import Search
+from app.schemas.types import MediaType
 
 
 class YyTest(_PluginBase):
@@ -17,7 +19,7 @@ class YyTest(_PluginBase):
     # 主题色
     plugin_color = "#74AA9C"
     # 插件版本
-    plugin_version = "1.2"
+    plugin_version = "1.3"
     # 插件作者
     plugin_author = "yyned2501"
     # 作者主页
@@ -40,10 +42,7 @@ class YyTest(_PluginBase):
     def init_plugin(self, config: dict = None):
         if config:
             self._enabled = config.get("enabled")
-            self._proxy = config.get("proxy")
-            self._recognize = config.get("recognize")
-            self._openai_url = config.get("openai_url")
-            self._openai_key = config.get("openai_key")
+            self.search = Search()
 
     def get_state(self) -> bool:
         return self._enabled
@@ -61,89 +60,68 @@ class YyTest(_PluginBase):
         """
         return [
             {
-                'component': 'VForm',
-                'content': [
+                "component": "VForm",
+                "content": [
                     {
-                        'component': 'VRow',
-                        'content': [
+                        "component": "VRow",
+                        "content": [
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 6},
+                                "content": [
                                     {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'enabled',
-                                            'label': '启用插件',
-                                        }
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "enabled",
+                                            "label": "启用插件",
+                                        },
                                     }
-                                ]
+                                ],
                             },
-                            {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
-                                    {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'proxy',
-                                            'label': '使用代理',
-                                        }
-                                    }
-                                ]
-                            },
-                            {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 4
-                                },
-                                'content': [
-                                    {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'recognize',
-                                            'label': '辅助识别',
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
+                        ],
                     },
-                ]
+                ],
             }
         ], {
             "enabled": False,
-            "proxy": False,
-            "recognize": False,
         }
 
     def get_page(self) -> List[dict]:
         pass
 
-    @eventmanager.register(EventType.UserMessage)
-    def talk(self, event: Event):
+    def search_movies(self, title: str, year: str) -> List[dict]:
         """
-        监听用户消息，获取ChatGPT回复
+        查询模糊匹配的所有电影TMDB信息
         """
-        if not self._enabled:
-            return
-        if not self.openai:
-            return
-        text = event.event_data.get("text")
-        userid = event.event_data.get("userid")
-        channel = event.event_data.get("channel")
-        if not text:
-            return
-        response = self.openai.get_response(text=text, userid=userid)
-        if response:
-            self.post_message(channel=channel, title=response, userid=userid)
+        if not title:
+            return []
+        ret_infos = []
+        if year:
+            movies = self.search.movies(term=title, year=year, adult=True) or []
+        else:
+            movies = self.search.movies(term=title, adult=True) or []
+        for movie in movies:
+            if title in movie.get("title"):
+                movie["media_type"] = MediaType.MOVIE
+                ret_infos.append(movie)
+        return ret_infos
+
+    def search_tvs(self, title: str, year: str) -> List[dict]:
+        """
+        查询模糊匹配的所有电视剧TMDB信息
+        """
+        if not title:
+            return []
+        ret_infos = []
+        if year:
+            tvs = self.search.tv_shows(term=title, release_year=year, adult=True) or []
+        else:
+            tvs = self.search.tv_shows(term=title, adult=True) or []
+        for tv in tvs:
+            if title in tv.get("name"):
+                tv["media_type"] = MediaType.TV
+                ret_infos.append(tv)
+        return ret_infos
 
     @eventmanager.register(EventType.NameRecognize)
     def recognize(self, event: Event):
@@ -156,30 +134,35 @@ class YyTest(_PluginBase):
         if not title:
             return
         # 收到事件后需要立码返回，避免主程序等待
-        if not self._enabled \
-                or not self.openai \
-                or not self._recognize:
-            eventmanager.send_event(
-                EventType.NameRecognizeResult,
-                {
-                    'title': title
-                }
-            )
+        if not self._enabled:
+            eventmanager.send_event(EventType.NameRecognizeResult, {"title": title})
             return
         # 调用ChatGPT
-        response = self.openai.get_media_name(filename=title)
-        logger.info(f"ChatGPT辅助识别结果：{response}")
-        if response:
+        movie = self.search_movies(title)
+        tv = self.search_tvs(title)
+        if len(movie)>0:
             eventmanager.send_event(
                 EventType.NameRecognizeResult,
                 {
-                    'title': title,
-                    'name': response.get("title"),
-                    'year': response.get("year"),
-                    'season': response.get("season"),
-                    'episode': response.get("episode")
-                }
+                    "title": title,
+                    "name": movie[0].get("title"),
+                    "year": movie[0].get("year"),
+                    "season": movie[0].get("season"),
+                    "episode": movie[0].get("episode"),
+                },
             )
+        elif len(movie)>0:
+            eventmanager.send_event(
+                EventType.NameRecognizeResult,
+                {
+                    "title": title,
+                    "name": tv[0].get("title"),
+                    "year": tv[0].get("year"),
+                    "season": tv[0].get("season"),
+                    "episode": tv[0].get("episode"),
+                },
+            )
+
 
     def stop_service(self):
         """
